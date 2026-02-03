b4aba32de1668d49319fd46c6009ff560be5bc70
alunosNET <alunosnet@gmail.com>
2026-02-03 21:09:52 +0000

    Add project files.

---
+++ VariaveisAmbiente/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariaveisAmbiente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string variavel = "temp";
+            try
+            {
+                string temp = Environment.GetEnvironmentVariable(variavel);
+                if (string.IsNullOrEmpty(temp))
+                    throw new Exception("Erro");
+                Console.WriteLine(temp);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Não existe a {variavel}");
+            }
+        }
+    }
+}
